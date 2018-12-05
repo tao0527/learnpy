94ff1825753608a3c387b17acc0749426f84b7db
tao0527 <328108327@qq.com>
2018-12-05 18:54:06 +0800

    update

--- lclass/r_w.py
+++ lclass/r_w.py
@@ -1,5 +1,16 @@
+import json
+
+
 file_name = 'test.txt'
 
 with open(file_name) as file_object:
     for line in file_object:
         print(line.rstrip())
+
+
+username = input("What is your name?\n")
+
+file_name1 = 'username.json'
+with open(file_name1, 'w') as f_obj:
+    json.dump(username, f_obj)
+    print("We'll remember you when you come back, " + username + "!")
